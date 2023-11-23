8a42e6427f95062869fc51fe73902b32159b659e
Loghic <michalek.matej.loghin@gmail.com>
2023-11-23 22:36:55 +0100

    HeadAndTail

---
+++ Recursion/HeadAndTail/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        printf("%d",n);
+        fun(n-1);
+    }
+}
+
+void fun2(int n)
+{
+    if(n>0)
+    {
+        fun2(n-1);
+        printf("%d",n);
+    }
+}
+
+int main() {
+    int x = 3;
+
+    fun(x);
+    printf("\n");
+    fun2(x);
+    return 0;
+}
