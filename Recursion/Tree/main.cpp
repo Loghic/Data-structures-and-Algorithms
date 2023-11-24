41186455ca1ae7ea036c21ec3a070e6960b644ae
Loghic <michalek.matej.loghin@gmail.com>
2023-11-24 10:22:01 +0100

    Tree recursion

---
+++ Recursion/Tree/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void fun(int n)
+{
+    if (n > 0)
+    {
+        printf("%d ", n);
+        fun(n-1);
+        fun(n-1);
+    }
+}
+
+int main() {
+    int n = 5;
+
+    fun(3);
+
+    return 0;
+}
